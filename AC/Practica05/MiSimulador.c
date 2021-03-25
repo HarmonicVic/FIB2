881fc9c51af2146835802656da3bc01f2a9d1402
Victor <vvt98@hotmail.com>
2021-03-26 00:28:45 +0100

    Practica 5 AC done

--- AC/Practica05/MiSimulador.c
+++ AC/Practica05/MiSimulador.c
@@ -1,68 +1,89 @@
 #include "CacheSim.h"
+#include <stdio.h>
 
 /* Posa aqui les teves estructures de dades globals
  * per mantenir la informacio necesaria de la cache
  * */
+//creamos cache simulada de 128 filas (4Kb / 32b), donde se guardan los tags
+int cache[128];
+int byte_mask = 0x1F;
+int mc_mask = 0xFE0;
+int hit_c = 0;
+int miss_c = 0;
 
-
-
-
-/* La rutina init_cache es cridada pel programa principal per
+	/* La rutina init_cache es cridada pel programa principal per
  * inicialitzar la cache.
- * La cache es inicialitzada al començar cada un dels tests.
+ * La cache es inicialitzada al comenï¿½ar cada un dels tests.
  * */
-void init_cache ()
+	void init_cache()
 {
-    totaltime=0.0;
+	totaltime = 0.0;
 	/* Escriu aqui el teu codi */
-
-
-
+	//limpiamos todo el cache poniendolo a -1 (tambien serviria cualquier numero "imposible", como -2, -3, etc.)
+	for (int i = 0; i < 128; i++)
+		cache[i] = -1;
 }
 
-/* La rutina reference es cridada per cada referencia a simular */ 
-void reference (unsigned int address)
+/* La rutina reference es cridada per cada referencia a simular */
+void reference(unsigned int address)
 {
 	unsigned int byte;
-	unsigned int bloque_m; 
+	unsigned int bloque_m;
 	unsigned int linea_mc;
 	unsigned int tag;
-	unsigned int miss;	   // boolea que ens indica si es miss
-	unsigned int replacement;  // boolea que indica si es reemplaça una linia valida
-	unsigned int tag_out;	   // TAG de la linia reemplaçada
-	float t1,t2;		// Variables per mesurar el temps (NO modificar)
-	
-	t1=GetTime();
+	unsigned int miss;		  // boolea que ens indica si es miss
+	unsigned int replacement; // boolea que indica si es reemplaï¿½a una linia valida
+	unsigned int tag_out;	  // TAG de la linia reemplaï¿½ada
+	float t1, t2;			  // Variables per mesurar el temps (NO modificar)
+
+	t1 = GetTime();
 	/* Escriu aqui el teu codi */
 
+	//reseteamos todos los valores booleanos
+	miss = 0;
+	replacement = 0;
+	tag_out = 0;
 
+	//calculamos el byte, el bloque_m, el bloque_mc y el tag a partir de address
+	byte = address & 0b00000000000000000000000000011111;
+	tag = address >> 12;
+	bloque_m = address >> 7;
+	linea_mc = address & 0b00000000000000000000111111100000;
 
+	// byte = address%32;
+	// bloque_m = address/32;
+	// linea_mc = bloque_m%128;
+	// tag = bloque_m/128;
 
-
-
-
-
-
-
-
-
-
-
+	//si la linea del cache no corresponde al tag
+	if (cache[linea_mc] != tag)
+	{
+		miss = 1;
+		miss_c++;
+		//si la linea tiene un valor, significa que no esta vacia, es decir tiene un tag, es decir hay un reemplazo con el miss
+		if (cache[linea_mc] != -1)
+		{
+			replacement = 1;
+			tag_out = cache[linea_mc];
+		}
+		cache[linea_mc] = tag;
+	}
+	else
+		hit_c++;
 
 	/* La funcio test_and_print escriu el resultat de la teva simulacio
 	 * per pantalla (si s'escau) i comproba si hi ha algun error
-	 * per la referencia actual. També mesurem el temps d'execució
+	 * per la referencia actual. Tambï¿½ mesurem el temps d'execuciï¿½
 	 * */
-	t2=GetTime();
-	totaltime+=t2-t1;
-	test_and_print (address, byte, bloque_m, linea_mc, tag,
-			miss, replacement, tag_out);
+	t2 = GetTime();
+	totaltime += t2 - t1;
+	test_and_print(address, byte, bloque_m, linea_mc, tag,
+				   miss, replacement, tag_out);
 }
 
-/* La rutina final es cridada al final de la simulacio */ 
-void final ()
+/* La rutina final es cridada al final de la simulacio */
+void final()
 {
- 	/* Escriu aqui el teu codi */ 
-  
-  
+	/* Escriu aqui el teu codi */
+	printf("%d misses, %d hits", miss_c, hit_c);
 }
--- AC/Practica05/MiSimulador2.c
+++ AC/Practica05/MiSimulador2.c
@@ -3,67 +3,74 @@
 /* Posa aqui les teves estructures de dades globals
  * per mantenir la informacio necesaria de la cache
  * */
-
-
-
+int cache[2][64];
 
 /* La rutina init_cache es cridada pel programa principal per
  * inicialitzar la cache.
- * La cache es inicialitzada al començar cada un dels tests.
+ * La cache es inicialitzada al comenï¿½ar cada un dels tests.
  * */
-void init_cache ()
+void init_cache()
 {
-    totaltime=0.0;
+	totaltime = 0.0;
 	/* Escriu aqui el teu codi */
-
-
-
+	for (int i = 0; i < 2; ++i)
+	{
+		for (int j = 0; j < 64; ++j)
+		{
+			cache[i][j] = -1;
+		}
+	}
 }
 
-/* La rutina reference es cridada per cada referencia a simular */ 
-void reference (unsigned int address)
+/* La rutina reference es cridada per cada referencia a simular */
+void reference(unsigned int address)
 {
 	unsigned int byte;
-	unsigned int bloque_m; 
+	unsigned int bloque_m;
 	unsigned int conj_mc;
 	unsigned int via_mc;
 	unsigned int tag;
-	unsigned int miss;	   // boolea que ens indica si es miss
-	unsigned int replacement;  // boolea que indica si es reemplaça una linia valida
-	unsigned int tag_out;	   // TAG de la linia reemplaçada
-	float t1,t2;		// Variables per mesurar el temps (NO modificar)
-	
-	t1=GetTime();
+	unsigned int miss;		  // boolea que ens indica si es miss
+	unsigned int replacement; // boolea que indica si es reemplaï¿½a una linia valida
+	unsigned int tag_out;	  // TAG de la linia reemplaï¿½ada
+	float t1, t2;			  // Variables per mesurar el temps (NO modificar)
+
+	t1 = GetTime();
 	/* Escriu aqui el teu codi */
 
+	//reseteamos variables booleanas
+	miss = 0;
+	replacement = 0;
+	tag_out = 0;
 
+	//calculo de las variables a partir de address
 
-
-
-
-
-
-
-
-
-
-
-
+	//si en la supuesta posicion del tag no esta el tag que buscamos, es un miss
+	if (cache[via_mc][conj_mc] != tag)
+	{
+		miss = 1;
+		//si la supuesta posicion no estaba vacia, hay que reemplazar
+		if (cache[via_mc][conj_mc] != -1)
+		{
+			replacement = 1;
+			tag_out = cache[via_mc][conj_mc];
+		}
+		//insertamos el nuevo tag en la posicion
+		cache[via_mc][conj_mc] = tag;
+	}
 
 	/* La funcio test_and_print escriu el resultat de la teva simulacio
 	 * per pantalla (si s'escau) i comproba si hi ha algun error
-	 * per la referencia actual. També mesurem el temps d'execució
+	 * per la referencia actual. Tambï¿½ mesurem el temps d'execuciï¿½
 	 * */
-	t2=GetTime();
-	totaltime+=t2-t1;
-	test_and_print2 (address, byte, bloque_m, conj_mc, via_mc, tag,
-			miss, replacement, tag_out);
+	t2 = GetTime();
+	totaltime += t2 - t1;
+	test_and_print2(address, byte, bloque_m, conj_mc, via_mc, tag,
+					miss, replacement, tag_out);
 }
 
-/* La rutina final es cridada al final de la simulacio */ 
-void final ()
+/* La rutina final es cridada al final de la simulacio */
+void final()
 {
- 	/* Escriu aqui el teu codi */ 
-  
-  
+	/* Escriu aqui el teu codi */
 }
